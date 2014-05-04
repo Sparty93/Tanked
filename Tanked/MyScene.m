1f949b1531af879d9f822a14d5890c69ffde325a
Sparty93 <robertmarchiori@me.com>
2014-05-04 21:42:40 +1000

    Initial Upload

--- Tanked/MyScene.m
+++ Tanked/MyScene.m
@@ -14,19 +14,58 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
+        self.physicsWorld.gravity = CGVectorMake(0.0, -0.05f);
+        
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
+        for (int i = 1; i<2; i++) {
+            [self createFishToAttack];
+        }
+
     }
     return self;
 }
+
+-(void)createFishToAttack {
+        SKSpriteNode *attackFish = [SKSpriteNode spriteNodeWithImageNamed:@"tankedFish"];
+        attackFish.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMaxY(self.frame));
+        attackFish.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(40,40)];
+        attackFish.zPosition = 100;
+        attackFish.physicsBody.mass = -1;
+        attackFish.xScale = 1;
+        attackFish.yScale = 1;
+    
+        [self addChild:attackFish];
+    
+        //Next attack fish spawn
+    
+        /*static const CGFloat thrust = 0.12;
+    
+        CGFloat fishDirection = .5;
+        CGVector thrustVector = CGVectorMake(thrust*cosf(fishDirection),
+                                         thrust*sinf(fishDirection));*/
+    
+        [attackFish.physicsBody applyForce:CGVectorMake(200, 200)];
+    
+          [self runAction:[SKAction sequence:@[
+            [SKAction waitForDuration:5],
+            [SKAction performSelector:@selector(createFishToAttack) onTarget:self],
+            //[SKAction performSelector:@selector(createFishToAttack) onTarget:self],
+        ]]];
+    
+        /*
+        UIBezierPath *randomPath = [UIBezierPath bezierPath];
+        [randomPath moveToPoint:CGPointMake(20, 20)];
+        [randomPath addCurveToPoint:CGPointMake(50, 50)
+                  controlPoint1:CGPointMake(200, 200)
+                  controlPoint2:CGPointMake(300, 50)];
+
+        SKAction *forever = [SKAction followPath:CFBridgingRetain(randomPath) duration:5];
+         [attackFish runAction:forever];
+         */
+
+}
+
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
